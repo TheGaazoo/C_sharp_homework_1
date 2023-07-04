5c15a2e90d31976abeeae37b5f860a2a7fd2f1b4
gaazoo <gaazoo@yandex.ru>
2023-07-04 23:19:13 +0700

    Домашнее задание готово!

---
+++ HomeWork_04/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Четные числа в заданном диапазоне:");
+for (int i = 1; i <= a; i=i+1) 
+{
+ if ((i%2)==0)
+ Console.WriteLine(i);   
+}   
