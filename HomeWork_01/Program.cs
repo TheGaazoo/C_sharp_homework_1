3fc4fea240682a728a9d6fcfb1e78398084902ae
gaazoo <gaazoo@yandex.ru>
2023-07-04 23:05:33 +0700

    Готово три задания

---
+++ HomeWork_01/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) 
+    Console.WriteLine("Число № 1 больше числа № 2");
+else if (a == b)
+    Console.WriteLine("Число № 1 равно числу № 2");
+else
+    Console.WriteLine("Число № 2 больше числа № 1");
---
+++ HomeWork_02/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c) 
+    Console.WriteLine("Число № 1 больше остальных чисел");
+else if (a == b && a == c)
+    Console.WriteLine("Все числа равны");
+else if (b > a && b > c)
+    Console.WriteLine("Число № 2 больше остальных чисел");
+else
+    Console.WriteLine("Число № 3 больше остальных чисел");
